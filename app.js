f54c8effd3bf4b8682ae3d053e9b8fd74224d9b7
aj <andrewleonard1693@gmail.com>
2017-10-28 17:59:14 -0400

    changed host details to aws rds

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+// node package declarations
+var mysql      	= 	require('mysql'),
+	socket		= 	require('socketio'),
+	express		=	require('express');
+
+
+var connection = mysql.createConnection({
+  host     : 'db-instance.cgs2c9qhx8la.us-east-2.rds.amazonaws.com',
+  port	   : '3306'
+  user     : 'ajl246',
+  password : 'billiejean'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
