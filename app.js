6a4309c07f9a03d9bab6d7f26693ab01f7e0c053
aj <andrewleonard1693@gmail.com>
2017-10-28 18:14:12 -0400

    changed password

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
   host     : 'db-instance.cgs2c9qhx8la.us-east-2.rds.amazonaws.com',
   port	   : '3306',
   user     : 'ajl246',
-  password : 'billiejean'
+  password : 'db-master-password'
 });
  
 connection.connect(function(err) {
