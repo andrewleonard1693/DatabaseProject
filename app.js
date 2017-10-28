c97036bbb14cbd9100aa96067527355861212312
aj <andrewleonard1693@gmail.com>
2017-10-28 18:01:04 -0400

    typo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mysql      	= 	require('mysql'),
 
 var connection = mysql.createConnection({
   host     : 'db-instance.cgs2c9qhx8la.us-east-2.rds.amazonaws.com',
-  port	   : '3306'
+  port	   : '3306',
   user     : 'ajl246',
   password : 'billiejean'
 });
