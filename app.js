8c959977cd81ecb841791134cb71c5530765e129
aj <andrewleonard1693@gmail.com>
2017-10-28 23:58:24 -0400

    comment

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ connection.connect(function(err) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Eat ass jack!')
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
