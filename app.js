94550bff3d102487adcabae2ac1a043652c07cfd
aj <andrewleonard1693@gmail.com>
2017-10-30 16:46:53 -0400

    added passport local

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var mysql      	= 	require('mysql'),
 	socket		= 	require('socketio'),
 	express		=	require('express'),
 	app			=	express();
+
+//create the connection details to the mysql database
 var connection = mysql.createConnection({
   host     : 'db-instance.cgs2c9qhx8la.us-east-2.rds.amazonaws.com',
   port	   : '3306',
   user     : 'ajl246',
   password : 'db-master-password'
 });
- 
+//connect to db
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -21,10 +23,12 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+
+//route for homepage
 app.get('/', function (req, res) {
   res.send('Eat ass jack')
 })
-
+//node app is listening on port 8080
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
