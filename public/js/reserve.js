862ccb3d9d2ad2e732dd7630aeb1eaa97dcdc394
aj <andrewleonard1693@gmail.com>
2017-12-05 23:29:37 -0500

    fixed cc type radio buttons

--- public/js/reserve.js
+++ public/js/reserve.js
@@ -9,7 +9,7 @@ $(function() {
       });
       //function that fires when the user clicks apply to the date range
       $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
-          $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+          $(this).val(picker.startDate.format('YYYY-MM-DD') + ' / ' + picker.endDate.format('YYYY-MM-DD'));
       });
     //function that fires when the user clicks cancel on the date range
       $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
