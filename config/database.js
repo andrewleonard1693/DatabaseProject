8e46bd0d90f79f7b373accbab25b72de7b390b42
andrewleonard1693 <andrewleonard1693@gmail.com>
2018-09-17 21:15:03 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     'connection': {
-        'host': 'db-instance.cgs2c9qhx8la.us-east-2.rds.amazonaws.com',
-        'port': '3306',
-        'user': 'ajl246',
-        'password': 'db-master-password'
+        'host': '',
+        'port': '',
+        'user': '',
+        'password': ''
     },
 	'database': 'PristineHotels',
     'users_table': 'Customer'
